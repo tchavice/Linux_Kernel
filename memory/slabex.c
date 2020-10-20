31b78566889c2c177b418844066d5634bd5920fa
tchavice <tchavice@gmail.com>
2020-10-20 11:54:06 -0400

    memory

---
+++ memory/slabex.c
@@ -0,0 +1,93 @@
+/* focalpoint LKI */
+/* Lab7: Memory Management Lab */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/delay.h>
+#include <linux/kthread.h>
+#include <asm/string.h>
+
+#define DRIVER_AUTHOR "focalpoint"
+#define DRIVER_DESC   "Lab7"
+
+MODULE_LICENSE("GPL");           // Get rid of taint message by declaring code as GPL.
+
+/*  Or with defines, like this: */
+MODULE_AUTHOR(DRIVER_AUTHOR);    // Who wrote this module?
+MODULE_DESCRIPTION(DRIVER_DESC); // What does this module do?
+
+struct task_struct *ts;
+
+/* Global kmem_cache struct */
+static struct kmem_cache *my_cachep;
+
+int init(void);
+void cleanup(void);
+
+static void init_my_cache(void)
+{
+	/* initialize my_cachep using kmem_cache_create */
+	my_cachep = kmem_cache_create("my_cache", 32, 0, GFP_KERNEL, NULL);	
+
+	return;
+}
+
+void slab_ex(void)
+{
+	void *myobject;
+
+	/* validate kmem_cache structure */
+	printk("slabex: Cache name is %s\n", my_cachep->name);
+	printk("slabex: Cache object size is %d\n", kmem_cache_size(my_cachep));
+
+	/* allocate a new object into the cache using kmem_cache_alloc */
+
+	printk("slabex: Allocating object in my_cachep with GFP_KERNEL flag\n");
+	myobject = kmem_cache_alloc(my_cachep, GFP_KERNEL);
+	if(myobject)
+	{
+		printk("slabex: Object created, freeing ...\n");
+
+		/* Free myobject using kmem_cache_free */
+		kmem_cache_free(my_cachep, myobject);
+		printk("slabex: Object freed\n");
+	}
+	else
+	{
+		printk("slabex: kmem_cache_alloc failed!\n");
+	}
+}
+
+static void remove_my_cache(void)
+{
+	printk("slabex: destroying kmem cache\n");
+
+	/* destroy kmem_cache using kmem_cache_destroy */
+	/* check to see if the cache is valid first ... */
+	if(my_cachep){
+		kmem_cache_destroy(my_cachep);
+	}
+	return;
+}
+
+int init(void)
+{
+	printk(KERN_INFO "slabex: init_module() called\n");
+	printk("slabex: Initializing kmem cache\n");
+	init_my_cache();
+
+	slab_ex(); /* create and free objects within the cache */
+
+	return 0;
+}
+
+void cleanup(void)
+{
+	remove_my_cache();
+	printk(KERN_ALERT "Unloading slabex ...\n");
+}
+
+module_init(init);
+module_exit(cleanup);
+
